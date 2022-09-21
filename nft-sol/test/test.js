6b849134dc849e62b34c6b60b8701cb379226cb7
OdionOseiwe <oseiweifebhor@gmail.com>
2022-09-21 17:06:26 +0100

    updated nft-sol

--- nft-sol/test/test.js
+++ nft-sol/test/test.js
@@ -40,5 +40,41 @@ describe("NFT", function () {
       
       await market.listnft(price,nft.address,1,{value:listing});
     })
-  })
+  });
+  describe("user come to buy nft", function(){
+    it("click on the nft and put the tokenid", async function(){
+      const { owner,market , nft, otherAccount} = await loadFixture(deployment);
+      await nft.mint(owner.address);
+
+      // // set listingprice 
+      let listing= ethers.utils.parseEther("0.5");
+      let listingprice = await market.setlistingprice(listing);
+
+      console.log(listingprice)
+
+      const  price = ethers.utils.parseEther("0.5");
+      // approve the marketplace
+      await nft.approve(market.address, 1);
+      // list market item
+      await market.listnft(price,nft.address,1,{value:listing});
+      // balance before
+      const balanceofsellerbefore= await ethers.provider.getBalance(owner.address);
+      console.log("this is the sellers balance before transfer", balanceofsellerbefore);
+      // buy an item
+      const buy =  ethers.utils.parseEther("0.5");
+      await market.connect(otherAccount).buynft(1, {value: buy});
+      // get balance of buyer
+      let balanceofbuyer = await nft.balanceOf(otherAccount.address);
+      console.log(balanceofbuyer ,"nft tokenid");
+      // get balance of seller
+      const balanceofseller= await ethers.provider.getBalance(owner.address);
+      console.log("thi is the sellers balance", balanceofseller);
+    });
+  });
+  describe("setprice", function(){
+    it("setslistingprice", async function(){
+      const { market, otherAccount} = await loadFixture(deployment);
+      await expect( market.connect(otherAccount).setlistingprice(2)).to.be.revertedWith("not owner");
+    })
+  });
 });
